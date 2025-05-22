70e5d8200a3baf825aeb35bf8903797d6fe68aa6
jhoaking <peredoroj@gmail.com>
2025-05-22 18:09:29 -0400

    cors helmet agregados

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import { authRouter } from 'routes/auth.routes';
-import { errorHandler } from 'middlewares/errorHandler';
-
-
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import { authRouter } from "routes/auth.routes";
+import { errorHandler } from "middlewares/errorHandler";
 export const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3150",
+      "http://localhost:3000",
+      "http://localhost:4000",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/user', authRouter);
+app.use("/user", authRouter);
 
-
-app.use(errorHandler)
+app.use(errorHandler);
